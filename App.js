0a691752d3291ad134eec10886eaaeba220f5522
akshaymalik1 <shreyas.srivastava@partner.vas.com>
2024-08-19 13:03:35 +0530

    first code ep1

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+  "h4",
+  {},
+  "Hello akshay malik this is coming from react"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const abc = root.render(heading);
+
+console.log(abc);
